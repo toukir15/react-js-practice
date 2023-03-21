e4bd788e3fd4b7b9853a5949ecdd7be72e9fc576
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-22 01:44:43 +0600

    fetch data in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h1>External Users</h1>
+      {/* <p>{users.name}</p> */}
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div>
+      <h3>name:{props.name}</h3>
+      <p>email:{props.email}</p>
+    </div>
+  )
+}
+
 function Counter() {
   const [count, setCount] = useState(55)
   const increaseCount = () => setCount(count + 1);
