81b15be54f3e0172a5ceb5a1b828319d23bd9199
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-21 23:20:16 +0600

    create counter by using react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: 'laptop', price: 100000 },
-    { name: 'mobile', price: 25000 },
-    { name: 'computer', price: 150000 },
-    { name: 'bike', price: 220000 },
-  ]
+
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
+      <Counter></Counter>
     </div>
   );
 }
+function Counter() {
+  const [count, setCount] = useState(55)
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1)
 
-function Product(props) {
   return (
-    <div className='product'>
-      <h1>Name:{props.name}</h1>
-      <h2>Price:{props.price}</h2>
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase  </button>
+      <button onClick={decreaseCount}>DecreaseCount</button>
     </div>
   )
 }
+
+// const products = [
+//   { name: 'laptop', price: 100000 },
+//   { name: 'mobile', price: 25000 },
+//   { name: 'computer', price: 150000 },
+//   { name: 'bike', price: 220000 },
+// ]
+// {
+//   products.map(product => <Product name={product.name} price={product.price}></Product>)
+// }
+
+// function Product(props) {
+//   return (
+//     <div className='product'>
+//       <h1>Name:{props.name}</h1>
+//       <h2>Price:{props.price}</h2>
+//     </div>
+//   )
+// }
 export default App;
